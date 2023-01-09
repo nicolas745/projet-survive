d6f75b9402149fb17e9251f9a6bcf9d0be23cc78
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2023-01-09 09:46:20 +0100

    deplacement

--- sketch.js
+++ sketch.js
@@ -1,34 +1,53 @@
-let personnage = {
-    postion: {
+class individu {
+    postion = {
         x: 0,
         y: 0
-    },
-    GetPosX: () => {
-        return personnage.postion.x
-    },
-    GetPosY: () => {
-        return personnage.postion.y
-    },
-    SetPosX: (x) => {
-        personnage.postion.x = x;
-    },
-    SetPosY: (y) => {
-        personnage.postion.y = y;
-    },
-    setPersonnage: () => {
+    }
+    constructor() {
+
+    }
+    get PosX() {
+        return this.postion.x
+    }
+    get PosY() {
+        return this.postion.y
+    }
+    set PosX(x) {
+        if (0 <= x && x <= 640) {
+            this.postion.x = x;
+        }
+    }
+    set PosY(y) {
+        if (0 <= y && y <= 480) {
+            this.postion.y = y;
+        }
+    }
+    setPersonnage() {
         fill(255);
-        ellipse(personnage.GetPosX(), personnage.GetPosY(), 10, 10);
+        ellipse(this.PosX, this.PosY, 10, 10);
+    }
+    deplacement(val) {
+        if (keyIsDown(LEFT_ARROW)) {
+            this.PosX = this.PosX - val
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.PosX = this.PosX + val
+        }
+        if (keyIsDown(UP_ARROW)) {
+            this.PosY = this.PosY - val
+        }
+        if (keyIsDown(DOWN_ARROW)) {
+            this.PosY = this.PosY + val
+        }
     }
 }
+
+let personnage = new individu();
 function setup() {
     createCanvas(640, 480);
 }
-
 function draw() {
     background(220);
-    personnage.setPersonnage()
-}
-
-function keyPressed() {
-    console.log(value);
+    personnage.deplacement(5);
+    personnage.setPersonnage();
 }
